6657421da9484af3decb9138128a66616fb157d3
Moses Moletsane <mosesmoletsane17@gmail.com>
2023-07-18 13:15:22 +0200

    first commit

---
+++ greet.js
@@ -0,0 +1,7 @@
+export default function greet(name) {
+    return "Hello, " + name;
+  }
+  
+  console.log(greet('Moses'));
+
+
---
+++ test/greet.test.js
@@ -0,0 +1,14 @@
+import assert from "assert";
+import greet from "../greet.js";
+
+describe('The greet function', function(){
+
+    it('should greet Andrew correctly', function(){
+        assert.equal('Hello, Andrew', greet('Andrew'));
+    });
+    it('should greet Karen correctly', function(){
+        // this test will fail - can you fix it?
+        assert.equal('Hello, Karen', greet('André'));
+    });
+});
+
